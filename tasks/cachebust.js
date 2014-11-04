5b2b41d467545b56627ac04a911632fdfe143d7e
Daniel Furze <daniel@furzeface.com>
2014-11-04 19:15:10 +0000

    feat(Task): Adds functionality to skip over sources served from CDNs etc, closes #4

--- tasks/cachebust.js
+++ tasks/cachebust.js
@@ -11,11 +11,10 @@
  module.exports = function (grunt) {
 
   grunt.registerMultiTask('cachebust', 'Append a timestamp to your assets to bust that cache!', function () {
-
     var $ = require('cheerio'),
     MD5 = require('MD5');
 
-    // Merge task-specific and/or target-specific options with these defaults.
+    // Merge task-specific and/or target-specific options with these defaults
     var options = this.options({
       type: 'MD5'
     });
@@ -24,7 +23,7 @@
       var timestamp = new Date().getTime();
     }
 
-    // Iterate over all specified file groups.
+    // Iterate over all specified file groups
     this.files.forEach(function (file) {
       var contents = file.src.filter(function(filepath) {
         if (!grunt.file.exists(filepath)) {
@@ -37,27 +36,35 @@
         return grunt.file.read(filepath);
       }).join('');
 
-      var $scripts = $(contents).find('script'),
+      var protocolRegEx = /^http(s)?/,
+      $scripts = $(contents).find('script'),
       $styles = $(contents).find('link[rel=stylesheet]');
 
       // Loop the stylesheet hrefs
       for (var i = 0; i < $styles.length; i++) {
         var styleHref = $styles[i].attribs.href;
-        if (options.type === 'timestamp') {
-          contents = contents.replace(styleHref, styleHref + '?t=' + timestamp);
-        } else {
-          contents = contents.replace(styleHref, styleHref + '?hash=' + MD5(contents));
+
+        // Test for http(s) and don't cache bust if (assumed) served from CDN
+        if (!protocolRegEx.test(styleHref)) {
+          if (options.type === 'timestamp') {
+            contents = contents.replace(styleHref, styleHref + '?t=' + timestamp);
+          } else {
+            contents = contents.replace(styleHref, styleHref + '?hash=' + MD5(contents));
+          }
         }
       }
 
       // Loop the script srcs
       for (var i = 0; i < $scripts.length; i++) {
         var scriptSrc = $scripts[i].attribs.src;
-        if (options.type === 'timestamp') {
-          contents = contents.replace(scriptSrc, scriptSrc + '?t=' + timestamp);
-        } else {
-          grunt.log.writeln('lol');
-          contents = contents.replace(scriptSrc, scriptSrc + '?hash=' + MD5(contents));
+
+        // Test for http(s) and don't cache bust if (assumed) served from CDN
+        if (!protocolRegEx.test(scriptSrc)) {
+          if (options.type === 'timestamp') {
+            contents = contents.replace(scriptSrc, scriptSrc + '?t=' + timestamp);
+          } else {
+            contents = contents.replace(scriptSrc, scriptSrc + '?hash=' + MD5(contents));
+          }
         }
       }
 
