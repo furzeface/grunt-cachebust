ccac61c8170feb42fa5702ba21a091875ce9626b
Daniel Furze <daniel@furzeface.com>
2014-11-09 16:37:12 +0000

    feat(Task): Moves functionality over to use node cachebust module

--- tasks/cachebust.js
+++ tasks/cachebust.js
@@ -9,71 +9,31 @@
  'use strict';
 
  module.exports = function (grunt) {
-
   grunt.registerMultiTask('cachebust', 'Append a timestamp to your assets to bust that cache!', function () {
-    var $ = require('cheerio'),
-    MD5 = require('MD5');
+    var cachebust = require('cachebust');
 
     // Merge task-specific and/or target-specific options with these defaults
     var options = this.options({
       type: 'MD5'
     });
 
-    if (options.type === 'timestamp') {
-      var timestamp = new Date().getTime();
-    }
+    // Loop the files
+    this.files.forEach(function (file, next) {
+      var src = file.src[0],
+      dest = file.dest;
 
-    // Iterate over all specified file groups
-    this.files.forEach(function (file) {
-      var contents = file.src.filter(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        return grunt.file.read(filepath);
-      }).join('');
-
-      var protocolRegEx = /^http(s)?/,
-      $scripts = $(contents).find('script'),
-      $styles = $(contents).find('link[rel=stylesheet]');
-
-      // Loop the stylesheet hrefs
-      for (var i = 0; i < $styles.length; i++) {
-        var styleHref = $styles[i].attribs.href;
-
-        // Test for http(s) and don't cache bust if (assumed) served from CDN
-        if (!protocolRegEx.test(styleHref)) {
-          if (options.type === 'timestamp') {
-            contents = contents.replace(styleHref, styleHref + '?t=' + timestamp);
-          } else {
-            contents = contents.replace(styleHref, styleHref + '?hash=' + MD5(contents));
-          }
-        }
+      if (!grunt.file.exists(src)) {
+        grunt.log.warn('Source file "' + src + '" not found.');
+        return next();
       }
 
-      // Loop the script srcs
-      for (var i = 0; i < $scripts.length; i++) {
-        var scriptSrc = $scripts[i].attribs.src;
+      // Call the Node module
+      var processedContents = cachebust.busted(grunt.file.read(src), options);
 
-        // Test for http(s) and don't cache bust if (assumed) served from CDN
-        if (!protocolRegEx.test(scriptSrc)) {
-          if (options.type === 'timestamp') {
-            contents = contents.replace(scriptSrc, scriptSrc + '?t=' + timestamp);
-          } else {
-            contents = contents.replace(scriptSrc, scriptSrc + '?hash=' + MD5(contents));
-          }
-        }
-      }
-
-      // Write the destination file
-      grunt.file.write(file.dest, contents);
-
+      // Write the file
+      grunt.file.write(file.dest, processedContents);
       // Print a success message
       grunt.log.writeln('File "' + file.dest + '" busted!');
     });
-});
-
+  });
 };
